af54badecb674388a616895639dedbbe996e2594
Paul Wekesa <44134284+Namianya@users.noreply.github.com>
2022-12-06 20:39:26 +0300

    inti firebase

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
-import { createApp } from "vue";
+import { initializeApp } from "firebase/app";
 import { createPinia } from "pinia";
+import { createApp } from "vue";
+
 import App from "./App.vue";
 
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 import "./assets/index.css";
+import { firebaseConfig } from "./firebase/int";
+
 const pinia = createPinia();
+
+const firebaseapp = initializeApp(firebaseConfig);
+getAuth(firebaseapp);
+getFirestore(firebaseapp);
 
 const app = createApp(App);
 app.use(pinia);
