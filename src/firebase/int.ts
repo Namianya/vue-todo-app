a91090c6a5a25475b4a7cb03b849fa35f7a3791f
Paul Wekesa <44134284+Namianya@users.noreply.github.com>
2022-12-06 20:39:17 +0300

    firebase config

---
+++ src/firebase/int.ts
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAm3iLXUz-SsIdqlT8jb2KkaVtinfG2TuM",
+  authDomain: "decisive-fabric-243821.firebaseapp.com",
+  projectId: "decisive-fabric-243821",
+  storageBucket: "decisive-fabric-243821.appspot.com",
+  messagingSenderId: "269370862689",
+  appId: "1:269370862689:web:aa52e322991d6db8524e20",
+};
+
+export { firebaseConfig };
