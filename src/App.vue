7b6bfdc83d6b4bb66bfc84e503c86d13e9e3d3b0
Paul Wekesa <44134284+Namianya@users.noreply.github.com>
2022-12-10 21:52:49 +0300

    add to firebase

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div class="bg-slate-100 min-h-screen">
     <div class="flex w-full justify-center">
       <button
-        v-if="!user"
+        v-if="!loggedInUser"
         @click="signIn"
         class="font-semibold p-4 m-8 shadow-md rounded-md bg-white"
       >
@@ -10,13 +10,13 @@
       </button>
       <button
         v-else
-        @click="signOut"
+        @click="signUserOut"
         class="font-semibold p-4 m-8 shadow-md rounded-md bg-red-500 text-white"
       >
         Sign out
       </button>
     </div>
-    <div v-if="user" class="pt-10">
+    <div v-if="loggedInUser" class="pt-10">
       <AddFormComponent />
       <div class="pt-20 px-10">
         <p class="text-lg font-bold">All Todos</p>
@@ -31,28 +31,56 @@
 </template>
 
 <script setup lang="ts">
-import { getAuth, GoogleAuthProvider, signInWithPopup, User } from "@firebase/auth";
-import { storeToRefs } from "pinia";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithPopup,
+  User,
+} from "@firebase/auth";
+import { collection, getFirestore, onSnapshot, query, where } from "@firebase/firestore";
 import { ref, Ref } from "vue";
 import AddFormComponent from "./components/AddFormComponent.vue";
 import TodoComponent from "./components/TodoComponent.vue";
-import { useTodoStore } from "./store/todoStore";
+import { Todo } from "./types/todoType";
 
-const { todos } = storeToRefs(useTodoStore());
+// const { todos } = storeToRefs(useTodoStore());
 
 const auth = getAuth();
+const loggedInUser: Ref<User | null> = ref(auth.currentUser);
 
-const user: Ref<User | null> = ref(auth.currentUser);
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    loggedInUser.value = user;
+    getTodos();
+  } else {
+    loggedInUser.value = null;
+  }
+});
+
+const todos: Ref<Todo[]> = ref([]);
+const firestore = getFirestore();
+const coll = collection(firestore, "todos");
+function getTodos() {
+  const todosList: Todo[] = [];
+  onSnapshot(query(coll, where("userId", "==", auth.currentUser!.uid)), (q) => {
+    q.forEach((doc) => {
+      todosList.push(doc.data() as Todo);
+      console.table(doc.data() as Todo);
+      todos.value = todosList;
+    });
+  });
+}
 
 const signIn = () => {
   signInWithPopup(auth, new GoogleAuthProvider()).then((result) => {
-    user.value = result.user;
+    loggedInUser.value = result.user;
   });
 };
 
-const signOut = () => {
+const signUserOut = () => {
   auth.signOut().then(() => {
-    user.value = null;
+    loggedInUser.value = null;
   });
 };
 </script>
